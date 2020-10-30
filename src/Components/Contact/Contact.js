c188e57b30a4486a050312ba4303bb695d1088f4
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2020-10-30 23:13:26 +0600

    Inshallah, next time will be updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,40 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css'
-// import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 const Contact = () => {
+    const [message, setMessage] = useState(false);
     const handleSubmit = (evt) =>{
         evt.preventDefault();
-        console.log("Email Validation");
-        // emailjs.sendForm('protfolio30313', 'template_1uu7gml', e.target, 'YOUR_USER_ID')
-        // .then((result) => {
-        //     console.log(result.text);
-        // }, (error) => {
-        //     console.log(error.text);
-        // });
+        // console.log("Email Validation");
+        emailjs.sendForm('protfolio30313', 'template_1uu7gml', evt.target, 'user_FBbxAs5F2iR78ILK22cF3')
+        .then((result) => {
+            console.log(result.text);
+            setMessage(true);
+        }, (error) => {
+            console.log(error.text);
+            setMessage(false);
+        });
+        evt.target.reset();
     }
     return (
-        <div style={{height: '60.3vh'}} className="p-4 container mx-auto row justify-content-center">
+        <><div style={{height: '60.3vh'}} className="p-4 container mx-auto row justify-content-center">
             <div className="col-md-12">
                 <h2 className="text-warning text-center ">Contact Me</h2>
                 <div className="d-flex justify-content-center">
                     <form onSubmit={handleSubmit}>
                         <div className='form-group'>
                             <label><small>Name</small></label>
-                            <input className="form-control" type="text" name="name" placeholder="Your Name" />
+                            <input className="form-control" type="text" name="name" placeholder="Your Name" required/>
                         </div>
                         <div className='form-group'>
                             <label><small>Email</small></label>
-                            <input className="form-control" type="text" name="email" placeholder="Your email" />
+                            <input className="form-control" type="text" name="email" placeholder="Your email" required/>
                         </div>
                         <div className="form-group">
-                            <label>Description</label>
-                            <textarea className="form-control" name="description" rows={3} placeholder="Your Text Here..." />
+                            <label>Message</label>
+                            <textarea className="form-control" name="message" rows={3} placeholder="Text Here" required/>
                         </div>
-                        <button className="btn btn-outline-danger" type="submit">Submit</button>
+                        <button className="btn btn-outline-danger w-100 text-warning" type="submit">Submit</button>
                     </form>
                 </div>
             </div>
         </div>
+        {
+            message && <p className='message text-success text-center'>Your message is sent successfully!!!</p>
+        }
+        </>
     );
 };
 
